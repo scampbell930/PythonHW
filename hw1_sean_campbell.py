afa8b1a95a90310a2b32544eba6bd9911064ff25
Sean Campbell <66285083+scampbell930@users.noreply.github.com>
2022-09-01 12:45:49 -0600

    Add files via upload

---
+++ hw1_sean_campbell.py
@@ -0,0 +1,18 @@
+"""
+Homework 1
+Name: Sean Campbell
+Date: 9/2/22
+Description of you program: This program is a small security program that asks a series of
+questions to the user and will only display the secret information if all the answers are
+correct. This program uses many different python practices to achieve this goal.
+"""
+
+if __name__ == '__main__':
+
+    # Loop until all answers are correct
+    not_correct = True
+
+    while not_correct:
+
+        # Check if all answers are correct
+        if
