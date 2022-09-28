5e1f2b03ab2f6f9a3aaf1f1a39ba0a3df48f5f32
Sean Campbell <seancampbell0110@gmail.com>
2022-09-28 11:17:01 -0600

    tictactoe mostly done

--- hw3_sean_campbell_ex_4.py
+++ hw3_sean_campbell_ex_4.py
@@ -5,8 +5,52 @@ Date: 10/07/2022
 Description: FILL THIS IN
 """
 
+
+# Function that displays an input game board, formatted, on the command line
+def print_board(board: dict):
+    # Display top index values
+    print("%5d%4d%4d" % (0, 1, 2))
+
+    # Loop through board to display elements
+    for i in range(len(board)):
+        # Check if last element in row
+        if (i + 1) % 3 == 0:
+            # Display element with new line
+            print("| {} |".format(board[i]))
+        else:
+            # Check if row num need to be displayed
+            if i % 3 == 0:
+                # Display element
+                print("{} | {} ".format(i//3, board[i]), end="")
+            else:
+                # Display element
+                print("| {} ".format(board[i]), end="")
+
+
 if __name__ == '__main__':
     # List to hold board data
-    board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    board = {0: ' ', 1: ' ', 2: ' ', 3: ' ', 4: ' ', 5: ' ', 6: ' ', 7: ' ', 8: ' '}
 
-    PRINT BOARD AND USER INPUT
+    user_one = False
+
+    # Run the game until user quits
+    while True:
+        # Update user
+        user_one = not user_one
+
+        # Display game board and ask for input
+        print_board(board)
+
+        # Prompt user for input
+        print("\nEnter Horizontal Coordinate:")
+        x_coord = int(input())
+
+        print("Enter Vertical Coordinate:")
+        y_coord = int(input())
+
+        # Calculate index from coordinates
+        index = 3 * y_coord + x_coord
+
+        # Store user character in board
+        board[index] = 'X' if user_one else 'O'
+
